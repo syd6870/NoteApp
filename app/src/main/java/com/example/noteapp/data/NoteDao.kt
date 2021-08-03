47ba2623efbbfb65b02fb85050c237453b1b7b77
Talib Siddiqui <sayyedtalib5@gmail.com>
2021-08-04 00:58:16 +0530

    Implemented Searchbar with Room,Added FlexBox and Appbar Menu

--- app/src/main/java/com/example/noteapp/data/NoteDao.kt
+++ app/src/main/java/com/example/noteapp/data/NoteDao.kt
@@ -6,8 +6,11 @@ import kotlinx.coroutines.flow.Flow
 @Dao
 interface NoteDao {
 
-    @Query("SELECT * FROM note_table")
-    fun getNotes():Flow<List<Note>>
+    @Query("SELECT * FROM note_table WHERE title LIKE '%'||:searchQuery || '%' ORDER BY created DESC")
+    fun getNotes(searchQuery:String):Flow<List<Note>>
+
+    @Query("SELECT * FROM note_table WHERE folder LIKE '%'||:folderName || '%' ORDER BY created DESC")
+    fun getNotesByFolder(folderName:String):Flow<List<Note>>
 
     @Insert(onConflict=OnConflictStrategy.REPLACE)
     suspend fun insert(note: Note)
--- app/src/main/java/com/example/noteapp/ui/note/NoteViewModel.kt
+++ app/src/main/java/com/example/noteapp/ui/note/NoteViewModel.kt
@@ -4,9 +4,19 @@ import androidx.hilt.lifecycle.ViewModelInject
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.asLiveData
 import com.example.noteapp.data.NoteDao
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.flatMapLatest
+
 
 class NoteViewModel @ViewModelInject constructor(
     noteDao: NoteDao
 ) : ViewModel() {
-    val notes = noteDao.getNotes().asLiveData()
+
+    val searchQuery= MutableStateFlow("")
+
+    private val taskFlow=searchQuery.flatMapLatest {
+        noteDao.getNotes(it)
+    }
+
+    val notes = taskFlow.asLiveData()
 }
