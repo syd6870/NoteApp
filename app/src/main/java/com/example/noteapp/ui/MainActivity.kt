ae9920fef5666bb6502e5de666259cd01217f069
Talib Siddiqui <sayyedtalib5@gmail.com>
2021-08-03 14:16:25 +0530

    Added Navigation Graph

--- app/src/main/java/com/example/noteapp/ui/MainActivity.kt
+++ app/src/main/java/com/example/noteapp/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.noteapp
+package com.example.noteapp.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.noteapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
