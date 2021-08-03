915f4d44749eca67073af7c588463f85d061f654
Talib Siddiqui <sayyedtalib5@gmail.com>
2021-08-03 14:16:25 +0530

    Added Navigation Graph

---
+++ app/src/main/java/com/example/noteapp/ui/note/NotesFragment.kt
@@ -0,0 +1,7 @@
+package com.example.noteapp.ui.note
+
+import androidx.fragment.app.Fragment
+import com.example.noteapp.R
+
+class NotesFragment :Fragment(R.layout.fragment_notes) {
+}
---
+++ app/src/main/java/com/example/noteapp/ui/start/StartFragment.kt
@@ -0,0 +1,7 @@
+package com.example.noteapp.ui.start
+
+import androidx.fragment.app.Fragment
+import com.example.noteapp.R
+
+class StartFragment :Fragment(R.layout.fragment_start) {
+}
