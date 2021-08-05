ed9d1fb6e72e2cec0daacb5b2da3c6fdc2f06a55
Talib Siddiqui <sayyedtalib5@gmail.com>
2021-08-05 13:55:22 +0530

    Implemented Sort Feature on Notes

--- app/src/main/java/com/example/noteapp/data/NoteDao.kt
+++ app/src/main/java/com/example/noteapp/data/NoteDao.kt
@@ -1,13 +1,25 @@
 package com.example.noteapp.data
 
 import androidx.room.*
+import com.example.noteapp.ui.note.SortOrder
 import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface NoteDao {
 
-    @Query("SELECT * FROM note_table WHERE title LIKE '%'||:searchQuery || '%' ORDER BY created DESC")
-    fun getNotes(searchQuery:String):Flow<List<Note>>
+    fun getNotes(searchQuery: String,sortOrder: SortOrder,hideCompleted: Boolean):Flow<List<Note>> =
+        when(sortOrder)
+        {
+            SortOrder.BY_DATE->getNotesSortByDate(searchQuery, hideCompleted)
+            SortOrder.BY_NAME->getNotesSortByName(searchQuery, hideCompleted)
+        }
+
+
+    @Query("SELECT * FROM note_table WHERE (isCompleted!=:hideCompleted OR isCompleted=0) AND title LIKE '%'||:searchQuery || '%' ORDER BY title DESC")
+    fun getNotesSortByName(searchQuery:String,hideCompleted:Boolean):Flow<List<Note>>
+
+    @Query("SELECT * FROM note_table WHERE (isCompleted!=:hideCompleted OR isCompleted=0) AND title LIKE '%'||:searchQuery || '%' ORDER BY created DESC")
+    fun getNotesSortByDate(searchQuery:String,hideCompleted:Boolean):Flow<List<Note>>
 
     @Query("SELECT * FROM note_table WHERE folder LIKE '%'||:folderName || '%' ORDER BY created DESC")
     fun getNotesByFolder(folderName:String):Flow<List<Note>>
--- app/src/main/java/com/example/noteapp/ui/note/NoteViewModel.kt
+++ app/src/main/java/com/example/noteapp/ui/note/NoteViewModel.kt
@@ -5,6 +5,7 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.asLiveData
 import com.example.noteapp.data.NoteDao
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.flatMapLatest
 
 
@@ -12,11 +13,22 @@ class NoteViewModel @ViewModelInject constructor(
     noteDao: NoteDao
 ) : ViewModel() {
 
-    val searchQuery= MutableStateFlow("")
+    val sortOrder = MutableStateFlow(SortOrder.BY_DATE)
+    val hideCompleted = MutableStateFlow(false)
+    val searchQuery = MutableStateFlow("")
 
-    private val taskFlow=searchQuery.flatMapLatest {
-        noteDao.getNotes(it)
+    private val taskFlow = combine(
+        searchQuery,
+        sortOrder,
+        hideCompleted
+    ) { query, sortOrder, hideCompleted ->
+        Triple(query, sortOrder, hideCompleted)
     }
+        .flatMapLatest { (query, sortOrder, hideCompleted) ->
+            noteDao.getNotes(query, sortOrder, hideCompleted)
+        }
 
     val notes = taskFlow.asLiveData()
 }
+
+enum class SortOrder { BY_NAME, BY_DATE }
